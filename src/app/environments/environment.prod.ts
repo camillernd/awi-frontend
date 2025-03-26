fc4ae141efb2fee53927de48048fdc8520d4b1d6
Alex <alexx97354@gmail.com>
2025-03-26 09:08:07 +0100

    fix: url

--- src/app/environments/environment.prod.ts
+++ src/app/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 // environment.prod.ts (Production)
 export const environment = {
     production: true,
-    BACKEND_URL: 'http://ludi-iwa.cluster-ig4.igpolytech.fr'
+    BACKEND_URL: 'http://crafawibackend.cluster-ig4.igpolytech.fr'
   };
