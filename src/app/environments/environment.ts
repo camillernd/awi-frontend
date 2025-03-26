9f6674e2a14e95b0e98c5d94094849cf4e94402e
Alex <alexx97354@gmail.com>
2025-03-26 09:19:10 +0100

    fix: url

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,5 +1,5 @@
 // environment.ts (Development)
 export const environment = {
     production: false,
-    BACKEND_URL: 'http://localhost:5000'
+    BACKEND_URL: 'http://crafawibackend.cluster-ig4.igpolytech.fr'
   };
